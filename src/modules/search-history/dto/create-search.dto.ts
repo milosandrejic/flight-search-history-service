772435beb6e7b5793b7751a03fb106b3f835caa8
Milos Andrejic <milosandrejic1993@gmail.com>
2026-06-22 11:08:51 +0200

    request validation

--- src/modules/search-history/dto/create-search.dto.ts
+++ src/modules/search-history/dto/create-search.dto.ts
@@ -1,7 +1,23 @@
+import { IsString, IsNotEmpty } from "class-validator";
+
 export class CreateSearchDto {
+  @IsString()
+  @IsNotEmpty()
   userId: string;
+
+  @IsString()
+  @IsNotEmpty()
   sessionId: string;
+
+  @IsString()
+  @IsNotEmpty()
   origin: string;
+
+  @IsString()
+  @IsNotEmpty()
   destination: string;
+
+  @IsString()
+  @IsNotEmpty()
   departureDate: string;
 }
--- src/modules/session/dto/create-session.dto.ts
+++ src/modules/session/dto/create-session.dto.ts
@@ -1,5 +1,15 @@
+import { IsString, IsNotEmpty } from "class-validator";
+
 export class CreateSessionDto {
+  @IsString()
+  @IsNotEmpty()
   userId: string;
+
+  @IsString()
+  @IsNotEmpty()
   origin: string;
+
+  @IsString()
+  @IsNotEmpty()
   destination: string;
 }
